f445332f6ba7e53c86d96d63db6cee71db2bed4d
Muneeb <muneebrizwan20@gmail.com>
2024-06-26 20:35:09 +0500

    justchill

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ import threading
 import subprocess
 import webbrowser
 
+#Websites---------------------------------------------
 x=lambda:webbrowser.open_new_tab('http://google.com')
 t=threading.Thread(target=x)
 t.start()
@@ -9,5 +10,12 @@ x=lambda:webbrowser.open_new_tab('https://www.instagram.com/')
 t=threading.Thread(target=x)
 t.start()
 x=lambda:webbrowser.open_new_tab('https://www.facebook.com/')
+t=threading.Thread(target=x)
+t.start()
+#Applications------------------------------------------
+x=lambda:subprocess.Popen('C:\\Windows\\System32\\calc.exe')
+t=threading.Thread(target=x)
+t.start()
+x=lambda:subprocess.Popen('C:\\Windows\\System32\\notepad.exe')
 t=threading.Thread(target=x)
 t.start()
